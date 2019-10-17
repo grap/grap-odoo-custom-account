aaf6784d41c0e2e9809d228042cfb965071a83be
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-17 14:20:58 +0200

    [REF] grap_pos_change_sale_move: Black python code

--- grap_pos_change_sale_move/__openerp__.py
+++ grap_pos_change_sale_move/__openerp__.py
@@ -4,20 +4,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Point Of Sale - Change Sale Move Lines',
-    'version': '8.0.1.0.0',
-    'category': 'Point Of Sale',
-    'author': 'GRAP',
-    'summary': 'Make Sale accounting moves from PoS acceptable for accoutants',
-    'depends': [
-        'pos_pricelist',
+    "name": "Point Of Sale - Change Sale Move Lines",
+    "version": "8.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP",
+    "summary": "Make Sale accounting moves from PoS acceptable for accoutants",
+    "depends": ["pos_pricelist"],
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/account_account.xml",
+        "demo/account_tax_code.xml",
+        "demo/account_tax.xml",
+        "demo/product_product.xml",
     ],
-    'demo': [
-        'demo/res_groups.xml',
-        'demo/account_account.xml',
-        'demo/account_tax_code.xml',
-        'demo/account_tax.xml',
-        'demo/product_product.xml',
-    ],
-    'installable': False,
+    "installable": False,
 }
--- grap_pos_change_sale_move/models/pos_order.py
+++ grap_pos_change_sale_move/models/pos_order.py
@@ -16,7 +16,8 @@ class PosOrder(models.Model):
     def _prepare_date_sale_move_point_of_sale(self):
         context_tz = pytz.timezone(self.env.user.tz)
         timestamp = datetime.strptime(
-            self.date_order, tools.DEFAULT_SERVER_DATETIME_FORMAT)
+            self.date_order, tools.DEFAULT_SERVER_DATETIME_FORMAT
+        )
         utc_timestamp = pytz.utc.localize(timestamp, is_dst=False)
         tz_timestamp = utc_timestamp.astimezone(context_tz)
         return tz_timestamp.strftime("%Y-%m-%d")
