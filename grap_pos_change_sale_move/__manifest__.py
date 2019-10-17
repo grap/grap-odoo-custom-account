9b6a4dca8f8d60ae42047c6e6b149f2ccf97f5d9
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-17 14:20:58 +0200

    [MIG] grap_pos_change_sale_move: Migration to 12.0

--- grap_pos_change_sale_move/__manifest__.py
+++ grap_pos_change_sale_move/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Point Of Sale - Change Sale Move Lines",
-    "version": "8.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Point Of Sale",
     "author": "GRAP",
     "summary": "Make Sale accounting moves from PoS acceptable for accoutants",
@@ -17,5 +16,5 @@
         "demo/account_tax.xml",
         "demo/product_product.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
--- grap_pos_change_sale_move/tests/test_pos_change_sale_move.py
+++ grap_pos_change_sale_move/tests/test_pos_change_sale_move.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from datetime import datetime, timedelta
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestChangeSaleMove(TransactionCase):
