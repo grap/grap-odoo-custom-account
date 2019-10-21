d65a5d1bb5987d8982cd8e43f1b90fbe93fa4498
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-21 11:58:59 +0200

    [REF] grap_pos_change_payment_move: Black python code

--- grap_pos_change_payment_move/__openerp__.py
+++ grap_pos_change_payment_move/__openerp__.py
@@ -4,16 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Point Of Sale - Change Payment Move Lines',
-    'version': '8.0.1.0.0',
-    'category': 'Point Of Sale',
-    'author': 'GRAP',
-    'summary': 'Make Bank accounting moves from PoS acceptable for accoutants',
-    'depends': [
-        'point_of_sale',
-    ],
-    'demo': [
-        'demo/res_groups.xml',
-    ],
-    'installable': False,
+    "name": "Point Of Sale - Change Payment Move Lines",
+    "version": "8.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP",
+    "summary": "Make Bank accounting moves from PoS acceptable for accoutants",
+    "depends": ["point_of_sale"],
+    "demo": ["demo/res_groups.xml"],
+    "installable": False,
 }
--- grap_pos_change_payment_move/tests/test_pos_change_payment_move.py
+++ grap_pos_change_payment_move/tests/test_pos_change_payment_move.py
@@ -9,56 +9,70 @@ from openerp.tests.common import TransactionCase
 
 
 class TetsChangePaymentMove(TransactionCase):
-
     def setUp(self):
         super(TetsChangePaymentMove, self).setUp()
 
-        self.session_obj = self.env['pos.session']
-        self.order_obj = self.env['pos.order']
-        self.move_obj = self.env['account.move']
+        self.session_obj = self.env["pos.session"]
+        self.order_obj = self.env["pos.order"]
+        self.move_obj = self.env["account.move"]
 
-        self.pos_config = self.env.ref('point_of_sale.pos_config_main')
-        self.product_evian = self.env.ref('point_of_sale.evian_2l')
-        self.partner_agrolait = self.env.ref('base.res_partner_2')
-        self.cash_journal = self.env.ref('account.cash_journal')
+        self.pos_config = self.env.ref("point_of_sale.pos_config_main")
+        self.product_evian = self.env.ref("point_of_sale.evian_2l")
+        self.partner_agrolait = self.env.ref("base.res_partner_2")
+        self.cash_journal = self.env.ref("account.cash_journal")
 
     # Tools function section
     def _open_session(self):
-        return self.session_obj.create({'config_id': self.pos_config.id})
+        return self.session_obj.create({"config_id": self.pos_config.id})
 
     def _sale(self, session, partner, journal, qty, date_diff=0):
-        order = self.order_obj.create({
-            'session_id': session.id,
-            'partner_id': partner and partner.id or False,
-            'lines': [[0, False, {
-                'discount': 0,
-                'price_unit': 1,
-                'product_id': self.product_evian.id,
-                'qty': qty}]]
-        })
+        order = self.order_obj.create(
+            {
+                "session_id": session.id,
+                "partner_id": partner and partner.id or False,
+                "lines": [
+                    [
+                        0,
+                        False,
+                        {
+                            "discount": 0,
+                            "price_unit": 1,
+                            "product_id": self.product_evian.id,
+                            "qty": qty,
+                        },
+                    ]
+                ],
+            }
+        )
         if date_diff:
-            order.date_order =\
-                datetime.strptime(order.date_order, "%Y-%m-%d %H:%M:%S") +\
-                timedelta(days=date_diff)
+            order.date_order = datetime.strptime(
+                order.date_order, "%Y-%m-%d %H:%M:%S"
+            ) + timedelta(days=date_diff)
         self.order_obj.add_payment(
-            order.id, {'amount': qty, 'journal': journal.id})
+            order.id, {"amount": qty, "journal": journal.id}
+        )
 
-        order.signal_workflow('paid')
+        order.signal_workflow("paid")
         return order
 
     def _close_session(self, session):
-        session.signal_workflow('close')
+        session.signal_workflow("close")
 
     def _get_payment_move(self, session, journal, partner_id):
-        return self.move_obj.search([
-            ('ref', '=', session.name),
-            ('journal_id', '=', journal.id),
-            ('partner_id', '=', partner_id)])
+        return self.move_obj.search(
+            [
+                ("ref", "=", session.name),
+                ("journal_id", "=", journal.id),
+                ("partner_id", "=", partner_id),
+            ]
+        )
 
     def _get_first_move_line(self, move, is_debit):
         self.assertEquals(
-            len(move.line_id), 2,
-            "Payment moves from Point of Sale should have two lines.")
+            len(move.line_id),
+            2,
+            "Payment moves from Point of Sale should have two lines.",
+        )
         if move.line_id[0].debit and is_debit:
             return move.line_id[0]
         else:
@@ -82,17 +96,22 @@ class TetsChangePaymentMove(TransactionCase):
         self._close_session(session)
 
         payment_moves = self._get_payment_move(
-            session, self.cash_journal, False)
+            session, self.cash_journal, False
+        )
 
         self.assertEquals(
-            len(payment_moves), 1,
+            len(payment_moves),
+            1,
             "Anonymous PoS orders (or uninvoiced PoS Orders) should generate"
-            " a uniq payment move when closing the session")
+            " a uniq payment move when closing the session",
+        )
 
         bank_line = self._get_first_move_line(payment_moves[0], True)
         self.assertEquals(
-            bank_line.debit, 100,
-            "incorrect Debit value for 4 sales (50, -10, 20, 40)")
+            bank_line.debit,
+            100,
+            "incorrect Debit value for 4 sales (50, -10, 20, 40)",
+        )
 
     # Test Section
     def test_02_test_invoiced_orders(self):
@@ -107,40 +126,52 @@ class TetsChangePaymentMove(TransactionCase):
 
         # sale #3 with customer, with invoice
         order = self._sale(
-            session, self.partner_agrolait, self.cash_journal, 20)
+            session, self.partner_agrolait, self.cash_journal, 20
+        )
         order.action_invoice()
         # sale #4 with customer, with invoice
         order = self._sale(
-            session, self.partner_agrolait, self.cash_journal, 40)
+            session, self.partner_agrolait, self.cash_journal, 40
+        )
         order.action_invoice()
 
         self._close_session(session)
 
         anonymous_payment_moves = self._get_payment_move(
-            session, self.cash_journal, False)
+            session, self.cash_journal, False
+        )
 
         customer_payment_moves = self._get_payment_move(
-            session, self.cash_journal, self.partner_agrolait.id)
+            session, self.cash_journal, self.partner_agrolait.id
+        )
 
         self.assertEquals(
-            len(anonymous_payment_moves), 1,
+            len(anonymous_payment_moves),
+            1,
             "orders of a given partner, invoiced and not invoiced, should"
-            " generate an anonymous move and one with customer.")
+            " generate an anonymous move and one with customer.",
+        )
 
         self.assertEquals(
-            len(customer_payment_moves), 1,
+            len(customer_payment_moves),
+            1,
             "orders of a given partner, invoiced and not invoiced, should"
-            " generate an anonymous move and one with customer.")
+            " generate an anonymous move and one with customer.",
+        )
 
         bank_line = self._get_first_move_line(anonymous_payment_moves[0], True)
         self.assertEquals(
-            bank_line.debit, 11,
-            "incorrect Debit value for 2 uninvoiced orders (1, 11)")
+            bank_line.debit,
+            11,
+            "incorrect Debit value for 2 uninvoiced orders (1, 11)",
+        )
 
         bank_line = self._get_first_move_line(customer_payment_moves[0], True)
         self.assertEquals(
-            bank_line.debit, 60,
-            "incorrect Debit value for 2 invoiced orders (20, 40)")
+            bank_line.debit,
+            60,
+            "incorrect Debit value for 2 invoiced orders (20, 40)",
+        )
 
     # Test Section
     def test_03_move_per_day(self):
@@ -155,8 +186,11 @@ class TetsChangePaymentMove(TransactionCase):
         self._close_session(session)
 
         anonymous_payment_moves = self._get_payment_move(
-            session, self.cash_journal, False)
+            session, self.cash_journal, False
+        )
 
         self.assertEquals(
-            len(anonymous_payment_moves), 2,
-            "Closing session should generate 1 payment move per day.")
+            len(anonymous_payment_moves),
+            2,
+            "Closing session should generate 1 payment move per day.",
+        )
