36e1bf97b5a8ad7ca23e6d8ac4ec31f09bd48b43
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-21 11:59:00 +0200

    [MIG] grap_pos_change_payment_move: Migration to 12.0

--- grap_pos_change_payment_move/models/pos_order.py
+++ grap_pos_change_payment_move/models/pos_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,7 +5,7 @@
 import pytz
 from datetime import datetime
 
-from openerp import api, models, tools
+from odoo import api, models, tools
 
 
 class PosOrder(models.Model):
