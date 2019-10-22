7c805ad2b46dd1fc3e34d28c71428c4a0199db76
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-22 11:43:27 +0200

    [ADD] grap_pos_change_sale_move

---
+++ grap_pos_change_sale_move/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Point Of Sale - Change Sale Move Lines",
+    "version": "12.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP",
+    "summary": "Make Sale accounting moves from PoS acceptable for accoutants",
+    "depends": ["point_of_sale"],
+    "license": "AGPL-3",
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/account_account.xml",
+        "demo/account_tax.xml",
+        "demo/product_product.xml",
+    ],
+    "installable": True,
+}
