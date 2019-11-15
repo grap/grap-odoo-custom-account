ec7ba3a780d56555b72c8df38fd79dd89e81bf21
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-15 17:30:08 +0100

    [MIG][12.0] port module grap_l10n_fr

---
+++ grap_l10n_fr/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author: Quentin DUPONT <quentin.dupont@grap.coop>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "GRAP - Change l10n_fr",
+    "summary": "Custom changes of l10n_fr module for GRAP",
+    "version": "12.0.1.0.0",
+    "category": "Custom",
+    "author": "GRAP",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_fr",
+        "account_fiscal_position_type",
+        "account_fiscal_position_usage_group",
+        "account_product_fiscal_classification",
+    ],
+    "data": [
+        "data/account_account_template.xml",
+        "data/account_chart_template.xml",
+        "data/account_fiscal_position_template.xml",
+        "data/account_fiscal_position_tax_template.xml",
+        "data/account_product_fiscal_classification_template.xml",
+        "data/account_tax_template.xml",
+    ],
+    "installable": True,
+}
