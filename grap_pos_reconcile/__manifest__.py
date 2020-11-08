70c9e27d7eab99e710d8692944f4e90959b60c2b
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-11-08 20:52:50 +0100

    [ADD] grap_pos_reconcile

---
+++ grap_pos_reconcile/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Point Of Sale - Pos Reconcile",
+    "version": "12.0.1.0.1",
+    "category": "Point Of Sale",
+    "author": "GRAP",
+    "license": "AGPL-3",
+    "summary": "Reconcile PoS entries",
+    "depends": ["grap_pos_change_payment_move", "grap_pos_change_sale_move"],
+    "installable": True,
+}
