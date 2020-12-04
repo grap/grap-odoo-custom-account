2737eea64cd2928c51206da738d4dadb0e00e07e
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-12-04 16:59:48 +0100

    [FIX] ebp export. restore 'fiscal_analytic, cortesy @quentinDupont

--- grap_account_export_ebp/models/res_company.py
+++ grap_account_export_ebp/models/res_company.py
@@ -4,7 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResCompany(models.Model):
@@ -15,11 +16,25 @@ class ResCompany(models.Model):
         string="Enable Analytic in EBP",
         default=False,
         help="Check this box if you want to enable the analytic when"
-        " exporting in EBP. Note that this setting has will not been"
-        " taken into account for Companies that belong to a CAE",
+        " exporting in EBP.",
     )
 
     ebp_default_analytic_account_id = fields.Many2one(
         string="Default Analytic Account for EBP",
         comodel_name="account.analytic.account",
     )
+
+    @api.constrains("ebp_analytic_enabled", "fiscal_type")
+    def _ebp_check_analytic_mode_fiscal_type(self):
+        companies = self.filtered(
+            lambda x: x.fiscal_type in ["fiscal_child", "fiscal_mother"]
+            and x.ebp_analytic_enabled
+        )
+        if companies:
+            raise ValidationError(
+                _(
+                    "Unable to enable analytic for the following companies"
+                    " that are CAE or Integrated companies %s"
+                    % (", ".join([x.name for x in companies]))
+                )
+            )
