3d94826f032d050fc62783f63c5d2b35a528adc7
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-02 21:09:00 +0100

    [IMP] grap_custom_account_payment_workflow : do not make this workflow for PoS bank journal

--- grap_custom_account_payment_workflow/models/account_payment.py
+++ grap_custom_account_payment_workflow/models/account_payment.py
@@ -10,31 +10,37 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     is_checked = fields.Boolean(
-        string="Checked", compute="_compute_bank_is_checked",
-        inverse="_inverse_bank_is_checked", store=True)
+        string="Checked",
+        compute="_compute_bank_is_checked",
+        inverse="_inverse_bank_is_checked",
+        store=True,
+    )
 
     @api.depends("move_line_ids.move_id.is_payment_checked")
     def _compute_bank_is_checked(self):
         for payment in self:
             payment.is_checked = any(
-                payment.mapped("move_line_ids.move_id.is_payment_checked"))
+                payment.mapped("move_line_ids.move_id.is_payment_checked")
+            )
 
     def _inverse_bank_is_checked(self):
         for payment in self.filtered(
-                lambda x: x.payment_type != 'transfer'
-                and x.state != 'draft'):
-            payment.mapped("move_line_ids.move_id").write({
-                "is_payment_checked": payment.is_checked})
+            lambda x: x.payment_type != "transfer" and x.state != "draft"
+        ):
+            payment.mapped("move_line_ids.move_id").write(
+                {"is_payment_checked": payment.is_checked}
+            )
 
     def post(self):
         res = super().post()
         for payment in self:
             if (
                 payment.payment_type in ["outbound", "inbound"]
-                and payment.journal_id.type in ["cash", "bank"]
+                and payment.journal_id.type == "bank"
+                and not payment.journal_id.journal_user
                 and not self.env.user.has_group("account.group_account_manager")
             ):
-                payment.mapped("move_line_ids.move_id").write({
-                    "is_payment_checked": False
-                })
+                payment.mapped("move_line_ids.move_id").write(
+                    {"is_payment_checked": False}
+                )
         return res
