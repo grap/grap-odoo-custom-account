21a74cb8f27190d80ba5a6d32e8cb098c6df4a2d
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-12-14 14:42:49 +0100

    [IMP] grap_l10n_fr : preconfigure ebp_suffix and ebp_export_tax / ebp_code_no_tax for grap needs

--- grap_l10n_fr/tests/test_module.py
+++ grap_l10n_fr/tests/test_module.py
@@ -14,6 +14,16 @@ class TestModule(TransactionCase):
         )
         self.euro = self.env.ref("base.EUR")
 
+    def _get_account(self, company, code):
+        return self.env["account.account"].search(
+            [("code", "=", code), ("company_id", "=", company.id)]
+        )[0]
+
+    def _get_tax(self, company, description):
+        return self.env["account.tax"].search(
+            [("description", "=", description), ("company_id", "=", company.id)]
+        )[0]
+
     # Test Section
     def test_01_create_new_chart_of_account(self):
 
@@ -23,3 +33,17 @@ class TestModule(TransactionCase):
         )
         self.env.user.company_id = company.id
         self.french_chart_of_account.try_loading_for_current_company()
+
+        # Check that custom fields are correctly applied on expense accounts
+        account_601 = self._get_account(company, "601")
+        self.assertTrue(account_601.ebp_export_tax)
+        self.assertEquals(account_601.ebp_code_no_tax, "9")
+
+        # Check that custom fields are correctly applied on income accounts
+        account_707 = self._get_account(company, "707")
+        self.assertTrue(account_707.ebp_export_tax)
+        self.assertFalse(account_707.ebp_code_no_tax)
+
+        # Check that custom fields are correctly applied on taxes
+        tax_sale_20 = self._get_tax(company, "TVA-VT-20.0-HT")
+        self.assertTrue(tax_sale_20.ebp_suffix)
