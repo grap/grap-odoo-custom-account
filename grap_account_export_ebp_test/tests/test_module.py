18ce8ca5ef25dde74cf4f407b81fd5b4f43e8ba0
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-03-18 15:10:26 +0100

    [ADD] grap_account_export_ebp_partner : tests

--- grap_account_export_ebp_test/tests/test_module.py
+++ grap_account_export_ebp_test/tests/test_module.py
@@ -9,6 +9,7 @@ from odoo.tests.common import TransactionCase
 class TestModule(TransactionCase):
     def setUp(self):
         super().setUp()
+        self.ResPartner = self.env["res.partner"]
         self.WizardEbpExport = self.env["wizard.ebp.export"]
         self.WizardEbpUnexport = self.env["wizard.ebp.unexport"]
         self.WizardResPartnerAddSuffix = self.env["wizard.res.partner.add.suffix"]
@@ -73,3 +74,49 @@ class TestModule(TransactionCase):
             1,
             "It should not be possible to export a unposted move.",
         )
+
+    def test_04_test_wizard_partner_add_suffix(self):
+        partner_with_suffix = self.ResPartner.create(
+            {"name": "Partner With Suffix", "ebp_suffix": "SUFF"}
+        )
+        partner_a = self.ResPartner.create({"name": "A"})
+        partner_bb = self.ResPartner.create({"name": "BB"})
+        partner_ccc = self.ResPartner.create({"name": "CCC"})
+        partner_dddd = self.ResPartner.create({"name": "DDDD"})
+        partner_duplicate_1 = self.ResPartner.create({"name": "bobleponge"})
+        partner_duplicate_2 = self.ResPartner.create({"name": "bobleponge"})
+
+        wizard = self.WizardResPartnerAddSuffix.with_context(
+            active_ids=[
+                partner_with_suffix.id,
+                partner_a.id,
+                partner_bb.id,
+                partner_ccc.id,
+                partner_dddd.id,
+                partner_duplicate_1.id,
+                partner_duplicate_2.id,
+            ]
+        ).create({})
+
+        line_partner_with_suffix = wizard.line_ids.filtered(
+            lambda x: x.partner_id.id == partner_with_suffix.id
+        )
+        line_a = wizard.line_ids.filtered(lambda x: x.partner_id.id == partner_a.id)
+        line_bb = wizard.line_ids.filtered(lambda x: x.partner_id.id == partner_bb.id)
+        line_ccc = wizard.line_ids.filtered(lambda x: x.partner_id.id == partner_ccc.id)
+        line_dddd = wizard.line_ids.filtered(
+            lambda x: x.partner_id.id == partner_dddd.id
+        )
+        line_duplicate_1 = wizard.line_ids.filtered(
+            lambda x: x.partner_id.id == partner_duplicate_1.id
+        )
+        line_duplicate_2 = wizard.line_ids.filtered(
+            lambda x: x.partner_id.id == partner_duplicate_2.id
+        )
+        self.assertEqual(line_partner_with_suffix.ebp_suffix, "SUFF")
+        self.assertEqual(line_a.ebp_suffix, "A")
+        self.assertEqual(line_bb.ebp_suffix, "BB")
+        self.assertEqual(line_ccc.ebp_suffix, "CCC")
+        self.assertEqual(line_dddd.ebp_suffix, "DDDD")
+        self.assertEqual(line_duplicate_1.ebp_suffix, "BOBL")
+        self.assertEqual(line_duplicate_2.ebp_suffix, "BOB2")
