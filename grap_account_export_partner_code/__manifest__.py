7767f385ea95d361b8cf746447d0e5b125c26740
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 14:32:08 +0200

    [REF] new module grap_account_export_partner_code

---
+++ grap_account_export_partner_code/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "GRAP - Account Export - Partner Code",
+    "summary": "Add an account export code field on partner model",
+    "version": "12.0.1.0.1",
+    "category": "GRAP Custom",
+    "license": "AGPL-3",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom-account",
+    "depends": [
+        # GRAP
+        "grap_account_export",
+    ],
+    "external_dependencies": {"python": ["unidecode"]},
+    "data": [
+        "views/view_res_partner.xml",
+        "wizards/view_wizard_res_partner_add_export_code.xml",
+    ],
+    "demo": [
+        "demo/res_partner.xml",
+    ],
+    "installable": True,
+}
