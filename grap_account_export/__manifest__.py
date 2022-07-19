ee5756d7d4e9bcb3e4204ee8e798d9c591bd6081
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 13:37:06 +0200

    [REF] new module grap_account_export

---
+++ grap_account_export/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "GRAP - Account Export",
+    "summary": "Add a placeholder menu for Accounting Export",
+    "version": "12.0.1.0.1",
+    "category": "GRAP Custom",
+    "license": "AGPL-3",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom-account",
+    "depends": ["account"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "demo": [
+        "demo/res_groups.xml",
+    ],
+    "installable": True,
+}
---
+++ grap_account_export/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module extends the functionality of Accounting module
+to add a menu that is a placeholder for accounting export.
+
+The menu is added in "Invoicing > Accounting".
