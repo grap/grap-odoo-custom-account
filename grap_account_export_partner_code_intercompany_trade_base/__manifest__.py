9d20990380e30e3674301d1f5d1978dfe6eb6fa0
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-27 12:35:42 +0200

    [ADD] grap_account_export_partner_code_intercompany_trade_base : new glue module to allow accountant to set accounting_export_code on partners

---
+++ grap_account_export_partner_code_intercompany_trade_base/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "GRAP - Account Export - Partner Code - Intercompany Trade",
+    "version": "12.0.1.0.1",
+    "category": "GRAP Custom",
+    "author": "GRAP",
+    "website": "https://github.com/grap/grap-odoo-custom-account",
+    "license": "AGPL-3",
+    "depends": [
+        # GRAP
+        "intercompany_trade_base",
+        "grap_account_export_partner_code",
+    ],
+    "auto_install": True,
+    "installable": True,
+}
---
+++ grap_account_export_partner_code_intercompany_trade_base/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module is a glue module between "GRAP - Account export - Partner code"
+and "Intercompany Trade Base" module to allow accountant to set partner code on
+intercompany trade partners.
