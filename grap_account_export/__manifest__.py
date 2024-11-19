761e6c96c275bf79781998fe7e9c03550018c22c
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-11-20 00:18:59 +0100

    [MIG] grap_account_export: Migration to 16.0 (from 12.0)

--- grap_account_export/__manifest__.py
+++ grap_account_export/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "GRAP - Account Export",
     "summary": "Add a placeholder menu for Accounting Export",
-    "version": "12.0.1.0.2",
+    "version": "16.0.1.0.0",
     "category": "GRAP Custom",
     "license": "AGPL-3",
     "author": "GRAP",
