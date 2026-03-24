7954ecce67748e6b7fc21e8c01b213ff7787b2bd
Quentin Dupont <quentin.dupont@grap.coop>
2026-03-24 10:24:53 +0100

    improve tests

--- fermente_account_payment_workflow/tests/test_fermente_account_payment_workflow.py
+++ fermente_account_payment_workflow/tests/test_fermente_account_payment_workflow.py
@@ -28,12 +28,20 @@ class TestFermenteAccountPaymentWorkflow(TransactionCase):
         cls.AccountPayment = cls.env["account.payment"]
 
         # Create datas
-        cls.user_demo = cls.env["res.users"].create(
+        cls.user_non_account_manager = cls.env["res.users"].create(
             {
-                "name": "John",
-                "login": "test1",
+                "name": "Emmanuel Macron Mozart de la Finance",
+                "login": "macrotte",
                 "groups_id": [(6, 0, cls.env.ref("account.group_account_user").ids)],
-                "email": "john@yourcompany.example.com",
+                "email": "macron@oligarchie.fr",
+            }
+        )
+        cls.user_account_manager = cls.env["res.users"].create(
+            {
+                "name": "Sibylle",
+                "login": "sibylle",
+                "groups_id": [(6, 0, cls.env.ref("account.group_account_manager").ids)],
+                "email": "sibylle@notedefrais.fr",
             }
         )
 
@@ -46,11 +54,19 @@ class TestFermenteAccountPaymentWorkflow(TransactionCase):
         cls.income_account = cls._create_account("Income", "INC", "income")
         cls.expense_account = cls._create_account("Expense", "EXP", "expense")
 
-        cls.env["account.journal"].create(
+        cls.journal_bank = cls.env["account.journal"].create(
             {
-                "name": "Bank Journal",
+                "name": "Bank Journal Test",
                 "code": "BNKTEST",
                 "type": "bank",
+            }
+        )
+
+        cls.journal_cash = cls.env["account.journal"].create(
+            {
+                "name": "Cash Journal Test",
+                "code": "CASHTEST",
+                "type": "cash",
             }
         )
 
@@ -68,14 +84,6 @@ class TestFermenteAccountPaymentWorkflow(TransactionCase):
                 "property_account_income_id": cls.income_account.id,
                 "property_account_expense_id": cls.expense_account.id,
             }
-        )
-
-        cls.sale_invoice = cls._create_invoice("out_invoice")
-        cls.sale_invoice.action_post()
-        # WIth user demo that don't have account manager rights
-        cls.payment = cls._create_payment_from_invoice_with_user(
-            cls.sale_invoice,
-            cls.user_demo,
         )
 
     # -------------------------
@@ -108,8 +116,9 @@ class TestFermenteAccountPaymentWorkflow(TransactionCase):
         return move_form.save()
 
     @classmethod
-    def _create_payment_from_invoice_with_user(cls, invoice, user):
+    def _create_payment_from_invoice_with_user(cls, invoice, journal, user):
         action = invoice.action_register_payment()
+        action["context"]["default_journal_id"] = journal.id
 
         with Form(
             cls.env[action["res_model"]].with_context(**action["context"])
@@ -125,8 +134,62 @@ class TestFermenteAccountPaymentWorkflow(TransactionCase):
     # TEST
     # -------------------------
 
-    def test_01_account_move_check_booleans(self):
-        self.assertEqual(self.sale_invoice.payment_state, "paid")
-        payment = self.payment
+    # Non accountant AND Bank journal → False
+    def test_01_non_account_manager_journal_bank(self):
+        sale_invoice1 = self._create_invoice("out_invoice")
+        sale_invoice1.action_post()
+
+        payment = self._create_payment_from_invoice_with_user(
+            sale_invoice1,
+            self.journal_bank,
+            self.user_non_account_manager,
+        )
+
+        self.assertEqual(sale_invoice1.payment_state, "paid")
         self.assertFalse(payment.is_checked)
         self.assertFalse(payment.move_id.is_payment_checked)
+
+    # Non accountant BUT Cash journal → True
+    def test_02_non_account_manager_journal_cash(self):
+        sale_invoice2 = self._create_invoice("out_invoice")
+        sale_invoice2.action_post()
+
+        payment2 = self._create_payment_from_invoice_with_user(
+            sale_invoice2,
+            self.journal_cash,
+            self.user_non_account_manager,
+        )
+
+        self.assertEqual(sale_invoice2.payment_state, "paid")
+        self.assertTrue(payment2.is_checked)
+        self.assertTrue(payment2.move_id.is_payment_checked)
+
+    # Accountant and Bank journal → True
+    def test_03_account_manager_journal_bank(self):
+        sale_invoice3 = self._create_invoice("out_invoice")
+        sale_invoice3.action_post()
+
+        payment3 = self._create_payment_from_invoice_with_user(
+            sale_invoice3,
+            self.journal_bank,
+            self.user_account_manager,
+        )
+
+        self.assertEqual(sale_invoice3.payment_state, "paid")
+        self.assertTrue(payment3.is_checked)
+        self.assertTrue(payment3.move_id.is_payment_checked)
+
+    # Accountant and Cash journal → True
+    def test_04_account_manager_journal_cash(self):
+        sale_invoice4 = self._create_invoice("out_invoice")
+        sale_invoice4.action_post()
+
+        payment4 = self._create_payment_from_invoice_with_user(
+            sale_invoice4,
+            self.journal_cash,
+            self.user_account_manager,
+        )
+
+        self.assertEqual(sale_invoice4.payment_state, "paid")
+        self.assertTrue(payment4.is_checked)
+        self.assertTrue(payment4.move_id.is_payment_checked)
