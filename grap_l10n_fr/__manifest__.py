9db997de9ad4eb912a89a4e8376792c524fbe872
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-04-28 17:08:44 +0200

    [MIG] grap_l10n_fr: Migration to 16.0 (from 12.0)

--- grap_l10n_fr/__manifest__.py
+++ grap_l10n_fr/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "GRAP - Change l10n_fr",
     "summary": "Custom changes of l10n_fr module for GRAP",
-    "version": "12.0.2.1.0",
+    "version": "16.0.1.0.0",
     "category": "Custom",
     "author": "GRAP",
     "website": "https://github.com/grap/grap-odoo-custom-account",
